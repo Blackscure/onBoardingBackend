eb160d7628eec479bec4ac7f73352c3ff6a9b021
Blackscure <wekesabuyahi@gmail.com>
2025-09-25 11:32:47 +0300

    Created authentication app, registered it in settings, and set up the API folder with urls, serializers, and views.

---
+++ authentication/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+from role.models import Role
+
+class User(AbstractUser):
+ 
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    user_type = models.ForeignKey(Role, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return  f"{self.first_name} {self.last_name} {self.email}"
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
