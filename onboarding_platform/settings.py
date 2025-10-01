09de51f30851f9f4e5d10fab32b188f2739b2104
Blackscure <wekesabuyahi@gmail.com>
2025-10-01 09:15:51 +0300

    requrment file modified

--- onboarding_platform/settings.py
+++ onboarding_platform/settings.py
@@ -134,8 +134,12 @@ WSGI_APPLICATION = 'onboarding_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'onboarding_db'),
+        'USER': os.getenv('POSTGRES_USER', 'onboarding_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'onboarding_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
