62280caa254c63421050d94d3bb5e988bb394994
Blackscure <wekesabuyahi@gmail.com>
2025-10-02 16:34:47 +0300

    user added

--- forms/models.py
+++ forms/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 import uuid
+from django.conf import settings
+
 
 class Form(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="forms"
+    )
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Field(models.Model):
     FIELD_TYPES = [
@@ -38,3 +49,6 @@ class Field(models.Model):
     required = models.BooleanField(default=False)
     options = models.JSONField(blank=True, null=True)
     validation_rules = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.label} ({self.type})"
